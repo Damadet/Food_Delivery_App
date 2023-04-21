c3603639eb69d6725e8c3cb8a9d27f9237d47e26
Hope <futurehopesaviour@gmail.com>
2023-04-21 03:57:42 +0100

    Updated about and colour changes

--- front-end/src/components/About.jsx
+++ front-end/src/components/About.jsx
@@ -7,14 +7,13 @@ export const About = () => {
             <div className="p-24 grid grid-cols-2">
                 <div className="">
                     <h2 className="text-2xl font-medium">About Us</h2>
+                    <br />
                     <p className="text-lg">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-                    beatae! Doloribus fuga aperiam magni ipsum repellat voluptates
-                    itaque error, atque, exercitationem fugit ab, modi ut voluptatum
-                    sequi ad eum! Rerum! Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Minus quia suscipit deserunt, neque nemo veniam
-                    adipisci deleniti culpa dolor dolores omnis, rem veritatis assumenda
-                    eaque dignissimos ut, nam debitis numquam!
+                    Yummy Request was established in 2023 as a result of the need to bridge the gap between purchasing quality homemade delicacies and avail
+                    Our menu includes crisp, tasty, home-made Nigerian traditional delicacies and intercontinental dishes. Our food is solid and YUMMY. We offer a broad range of traditional and intercontinental menu to make the best choices.
+                    Make a order and try us out! We will come to you! 
+                    <br/>
+                    At Yummy Request, our energy, qualities and objectives depend on a certain something, the drive to satisfy our customers!
                     </p>
                 </div>
                 <div className="flex items-center justify-center">
--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -34,7 +34,7 @@ export const Header = ({ cartCount }) => {
     }, [])
 
     return (
-        <nav id="header" className="bg-black text-white">
+        <nav id="header" className="bg-white text-black font-bold">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="logo-wrapper pl-4 flex items-center">
                     <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
